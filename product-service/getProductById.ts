f74f6b65a7fb82ac181ffd1fe60c831402c87b59
Oleksandr_Panibraten <oleksandr_panibratenko@epam.com>
2020-11-06 00:50:08 +0200

    [AWS Services] Code review update

--- product-service/getProductById.ts
+++ product-service/getProductById.ts
@@ -2,17 +2,16 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
 import products from './db/productList.json';
+import { generateApiReponseHeaders } from './utils/generateApiReponseHeaders';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
+  console.log('getProductByIdHandler: Event: ', event);
   const id = event.pathParameters?.id;
   if (!id) {
     return {
       statusCode: 404,
-      body: 'Error: No id',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      }
+      body: 'Error: Mandatory path parameter is missed',
+      headers: generateApiReponseHeaders(),
     };
   }
 
@@ -21,20 +20,14 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   if (!product) {
     return {
       statusCode: 400,
-      body: `No any product with this id: ${id} `,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      }
+      body: `Can't find product with id: ${id} `,
+      headers: generateApiReponseHeaders(),
     };
   }
 
   return {
     statusCode: 200,
     body: JSON.stringify(product),
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    }
+    headers: generateApiReponseHeaders(),
   }
 }
--- product-service/getProductsList.ts
+++ product-service/getProductsList.ts
@@ -2,14 +2,13 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
 import products from './db/productList.json';
+import { generateApiReponseHeaders } from './utils/generateApiReponseHeaders';
 
-export const handler: APIGatewayProxyHandler = async () => {
+export const handler: APIGatewayProxyHandler = async (event) => {
+  console.log('getProductsListHandler: Event: ', event);
   return {
     statusCode: 200,
     body: JSON.stringify(products),
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    }
+    headers: generateApiReponseHeaders(),
   }
 }
